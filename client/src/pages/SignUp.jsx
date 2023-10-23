06b0e4adc5a8f46853c8c52453756174c07fc23b
suhaml <souhamlaihi95@gmail.com>
2023-10-23 23:01:07 +0200

    ntesti

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
+
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
