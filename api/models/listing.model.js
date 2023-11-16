bc82eeb388c284db53d97cf199f0aa710d2c30dc
AymenHammami <aymen.hammamicsft@gmail.com>
2023-11-16 03:14:29 +0100

    create listing ui

--- api/models/listing.model.js
+++ api/models/listing.model.js
@@ -1,87 +1,63 @@
-import mongoose from "mongoose";
-const listingSchema = new mongoose.Schema(
-    {
-        titre: {
-             type: String, 
-            required: true 
-        },
-        description: { 
-            type: String,
-            required: true 
-        },
-        category: {
-             type: String, 
-            required: true,
-            enum: {
-                values:[    
-                    
-                    'animaux',
-                    'emploi',
-                    'enfants',
-                    'immobilier',
-                    'loisir',
-                    'mode',
-                    'multimedia',
-                    'pour la maison',
-                    'sante',
-                    'service',
-                    'vehicules',
-                    'voyage'
-                 ]
-            }
-        },
-        prix: {
-             type: Number, 
-            required: true 
-        },
-        phone: { 
-            type: String,
-            required: true 
-        },
-        imageUrls: {
-             type: Array,
-             required: true
-            }, // Store image URLs array bc its more thaan one image
-        region: { 
-            type: String, 
-            required: true,
-            enum:{
-                values:[
-                    'ariana',
-                    'beja',
-                    'ben arous',
-                    'bizerte',
-                    'gabes',
-                    'gafsa',
-                    'jendouba',
-                    'kairouan',
-                    'kasserine',
-                    'kebili',
-                    'le kef',
-                    'mahdia',
-                    'manouba',
-                    'medenine',
-                    'monastir',
-                    'nabeul',
-                    'sfax',
-                    'sidi bouzid',
-                    'siliana',
-                    'sousse',
-                    'tataouine',
-                    'tozeur',
-                    'tunis',
-                    'zaghouan'
-                ]
-            }
-        },
-        userRef:{
-                type: String, 
-                required: true 
-        },
-}, {timestamps:true}
+import mongoose from 'mongoose';
 
+const listingSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    regularPrice: {
+      type: Number,
+      required: true,
+    },
+    discountPrice: {
+      type: Number,
+      required: true,
+    },
+    bathrooms: {
+      type: Number,
+      required: true,
+    },
+    bedrooms: {
+      type: Number,
+      required: true,
+    },
+    furnished: {
+      type: Boolean,
+      required: true,
+    },
+    parking: {
+      type: Boolean,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    offer: {
+      type: Boolean,
+      required: true,
+    },
+    imageUrls: {
+      type: Array,
+      required: true,
+    },
+    userRef: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
 
 const Listing = mongoose.model('Listing', listingSchema);
 
-export default Listing ;
+export default Listing;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,8 +9,8 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/' >
         <h1 className='font-bold text-base sm:text-3xl flex flex-wrap '>
-            <span className='text-slate-500'>Tak</span>
-            <span className='text-slate-700'>Tak</span>
+            <span className='text-slate-500'>Cozy</span>
+            <span className='text-slate-700'>Corner</span>
         </h1>
         </Link>
         <form className='bg-slate-100 p-3 rounded-lg flex items-center '>
