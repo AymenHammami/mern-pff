2e80c9489d6104743578453ae4122e67d148d5c3
AymenHammami <aymen.hammamicsft@gmail.com>
2023-11-19 05:24:26 +0100

    delete user listing fonctionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import  express  from "express";
-import { createListing, getAllCategories, getAllRegions } from "../controllers/listing.controller.js";
+import { createListing, deleteListing, getAllCategories, getAllRegions } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,6 +7,9 @@ const router = express.Router();
 router.post('/create', verifyToken, createListing);
 router.get('/categories', getAllCategories);
 router.get('/regions', getAllRegions);
+router.delete('/delete/:id', verifyToken, deleteListing);
+
+
 
 
 export default router;
